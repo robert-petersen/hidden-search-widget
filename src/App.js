3734cb16fda52e9f7366ce7f13333dfcf4d53ec8
Robert Petersen <robert.petersen808@gmail.com>
2022-08-12 10:46:39 -0500

    Created hidden search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import {FaSearch} from "react-icons/fa";
+import {useState} from "react";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <div className="App">
-      
+      <div className={ isActive ? "active search" : "search" }>
+        <input type="text" className="input" placeholder="Search..."></input>
+        <button 
+          className="btn"
+          onClick={() => {setIsActive(!isActive)}}
+        ><FaSearch className="icon"/></button>
+      </div>
     </div>
   );
 }
